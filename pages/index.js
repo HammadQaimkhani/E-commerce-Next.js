2c26ff32dd53739465419e69408582323fd3ee53
HAMMAD QAIMKHNAI <62787660+HammadQaimkhani@users.noreply.github.com>
2023-04-28 19:15:40 +0500

    Gooogle Auth

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,26 @@
+import { signIn, signOut, useSession } from "next-auth/react";
 export default function Home() {
-  return (
-    <div className='bg-blue-900 w-screen h-screen flex justify-center items-center'>
-      <div className=' w-full text-center'>
-        <button className='bg-white  text-black p-2 px-4 rounded-lg'>
-          Login with Google
-        </button>
+  const { data: session } = useSession();
+  if (!session) {
+    return (
+      <div className='bg-blue-900 w-screen h-screen flex justify-center items-center'>
+        <div className=' w-full text-center'>
+          <button
+            onClick={() => signIn("google")}
+            className='bg-white  text-black p-2 px-4 rounded-lg'>
+            Login with Google
+          </button>
+        </div>
       </div>
+    );
+  }
+  return (
+    <div>
+      "logged in Successfully!!" {session.user.email}
+      <br />
+      {session.user.name}
+      <br />
+      <button onClick={() => signOut()}>signOut</button>
     </div>
   );
 }
