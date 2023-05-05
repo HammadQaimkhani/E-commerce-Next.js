b6d4198655348678c3362610e648e7400c93118e
HAMMAD QAIMKHNAI <62787660+HammadQaimkhani@users.noreply.github.com>
2023-05-05 20:28:57 +0500

    get product images

--- components/ProductsForm.js
+++ components/ProductsForm.js
@@ -37,6 +37,21 @@ const ProductForm = ({
     setGotoProducts(true);
   };
 
+  // upload Images.
+  const uploadImages = async e => {
+    const files = e.target?.files;
+    if (files?.length > 0) {
+      const data = new FormData();
+      for (const file of files) {
+        data.append("files", file);
+      }
+      const response = await axios.post("/api/upload", data, {
+        headers: { "Content-Type": "multipart/form-data" },
+      });
+      console.log(response.data);
+    }
+  };
+
   // set route after the click the button
   if (gotoProducts) {
     router.push("/products");
@@ -53,6 +68,27 @@ const ProductForm = ({
         placeholder='product name'
         className='focus:border-blue-900'
       />
+      {/* Images... */}
+      <label>Photos</label>
+      <div className='mb-2'>
+        <label className='flex gap-1 border-2 bg-gray-100 border-gray-200 w-32 h-32 items-center rounded-lg justify-center text-gray-500 cursor-pointer border-dashed '>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth={1.5}
+            stroke='currentColor'
+            className='w-6 h-6'>
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              d='M9 8.25H7.5a2.25 2.25 0 00-2.25 2.25v9a2.25 2.25 0 002.25 2.25h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25H15m0-3l-3-3m0 0l-3 3m3-3V15'
+            />
+          </svg>
+          <div>Uploads</div>
+          <input type='file' className='hidden' onChange={uploadImages} />
+        </label>
+      </div>
 
       {/* description */}
       <label>Description</label>
---
+++ pages/api/upload.js
@@ -0,0 +1,28 @@
+import { mongooseConnect } from "@/lib/mongoose";
+import { Products } from "@/model/products";
+import multiparty from "multiparty";
+import { resolve } from "styled-jsx/css";
+
+const handle = async (req, res) => {
+  await mongooseConnect();
+  const { method } = req;
+  const { fields, files } = await new Promise((resolve, reject) => {
+    const form = new multiparty.Form();
+    form.parse(req, (err, fields, files) => {
+      if (err) throw reject(err);
+      resolve({ fields, files });
+    });
+  });
+
+  console.log(files.length);
+  res.json("ok");
+  if (method === "POST") {
+  }
+};
+
+export default handle;
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
